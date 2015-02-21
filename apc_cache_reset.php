57385dc7047bbd6c37546c3c8637beb7a6169f80
sjlangley <Olivi@506>
2015-02-21 21:51:08 +1100

    Reset just the oath token keys.

--- apc_cache_reset.php
+++ apc_cache_reset.php
@@ -1,11 +1,17 @@
+<html>
+<PRE>
 <?php
 $key_names = [
   '_ah_app_identity_:https://www.googleapis.com/auth/devstorage.read_only',
   '_ah_app_identity_:https://www.googleapis.com/auth/devstorage.read_write',
 ];
 
+
 foreach($key_names as $key) {
   print 'Resetting: ' . $key . ' ';
   print apc_delete($key) ? 'OK' : 'FAILED';
   print PHP_EOL;
 }
+?>
+</PRE>
+</html>
